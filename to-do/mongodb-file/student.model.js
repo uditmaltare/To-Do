a975e04c348d566454bb1130af32ffdffe4ce46a
Udit-Maltare <162106089+uditmaltare@users.noreply.github.com>
2025-03-17 20:14:52 +0530

    Delete to-do directory

--- to-do/mongodb-file/student.model.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-const studentSchema = new mongoose.Schema({
-    name: String,
-    surname: String,
-    age: Number
-}, { timestamps: true })
-export const Student = mongoose.model('Student', studentSchema)
